bb80a893c72c57bf87ada4d70fed210644f1712c
Thauany Celestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-05-29 17:23:28 -0400

    d

--- JS/contato.js
+++ JS/contato.js
@@ -1,6 +1,17 @@
 const inputs = document.querySelectorAll("[required]");
 inputs.forEach( (elemento)=> {
     elemento.addEventListener("blur", (evento)=>{
-        console.log("sair")
+        validaCampo(evento.target)
     });
 });
+
+function validaCampo(campo){
+    const msnErro = campo.parentNode.querySelector("[data-erro]");
+    if(campo.name === "Nome"){
+        if(campo.value.length<5){
+            msnErro.textContent = "Digite o nome completo";
+        }else{
+            msnErro.textContent= "";
+        }
+    }
+}
